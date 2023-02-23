581052cd8e73c755a2b9b36ac354e4ea540851b1
Codehash001 <codehashblockchain@gmail.com>
2023-02-23 18:51:53 +0530

    css fix

--- components/navbar.js
+++ components/navbar.js
@@ -14,14 +14,14 @@ export default function Navbar (){
   
     return (
             <>
-            <div className='top-0 sticky z-10 filter drop-shadow-md w-screen h-auto lg:px-16 font-Gotham '>
+            <div className='top-0 sticky z-10 filter drop-shadow-md w-screen h-auto font-Gotham '>
               <div className='flex flex-col items-center w-full'>
 
-                <div className='w-full h-[40px] bg-gray-900 py-4'>
+                <div className='w-full bg-gray-900 py-4'>
                   <h1 className='text-center font-semibold uppercase text-white'>Mint is Live</h1>
                 </div>
 
-                <div className='flex flex-col w-full h-[60px]'>
+                <div className='flex flex-row w-full h-[60px] lg:px-16 '>
                   <div className='w-[40%] flex items-center p-4 justify-between'>
                     <h1 className='text-center font-medium'>Home</h1>
                     <h1 className='text-center font-medium'>Roadmap</h1>
@@ -34,7 +34,7 @@ export default function Navbar (){
                   <div className='w-[40%] flex items-center p-4 justify-between'>
                     <h1 className='text-center font-medium'>Team</h1>
                     <h1 className='text-center font-medium'>FAQ</h1>
-                    <button className='text-center p-4 tracking-widest uppercase bg-gray-500 text-white font-semibold rounded-full'>MINT</button>
+                    <button className='text-center py-4 px-10 tracking-widest uppercase bg-gray-500 text-white font-semibold rounded-full'>MINT</button>
                   </div>
 
                 </div>
