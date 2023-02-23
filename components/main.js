cef2f8bf185b8da94692d1bead5eab51b4e64865
Codehash001 <codehashblockchain@gmail.com>
2023-02-23 20:22:44 +0530

    css fix

--- components/main.js
+++ components/main.js
@@ -3,11 +3,11 @@ import {Link} from 'react-scroll/modules';
 export default function Home(){
 
     return (
-        <div className="h-screen font-Gotham" id='home'data-aos="fade">
+        <div className="pt-[80px] h-screen font-Gotham" id='home'data-aos="fade">
             <div className='flex flex-col items-center justify-center'>
                 <h1 className='font-bold text-[60px]'>Home Page</h1>
                 <button className='text-center py-2 px-10 tracking-widest uppercase bg-gray-500 text-white font-bold rounded-full text-[30px] mt-10'>MINT NOW</button>
-                <p className='text-white font-medium text-[20px] mt-10'>
+                <p className='text-gray-300 font-medium text-[20px] mt-10 text-center'>
                     Consectetur irure amet pariatur exercitation culpa ut.
                     <br/>
                     Lorem Ipsum consectetur irure amet pariatur exercitation culpa ut.
