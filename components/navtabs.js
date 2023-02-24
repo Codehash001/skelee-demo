2ec71b7c3af1e289f39481a7c9f6711c0cdd0fbc
Codehash001 <codehashblockchain@gmail.com>
2023-02-24 19:06:24 +0530

    setup toggle states

--- components/navtabs.js
+++ components/navtabs.js
@@ -6,7 +6,14 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 export default function Navtabs (){
 
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false)
+
+
+  const [toggleState, setToggleState] = useState(1);
+
+   const toggleTab = (index) =>{
+    setToggleState (index);
+   }
 
   const handleNav = () => {
     setNav(!nav);
@@ -32,17 +39,49 @@ export default function Navtabs (){
 
                 <div className='w-[50%]'>
                   <div className='flex items-center justify-end'>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mr-2 cursor-pointer'>Mint</div>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>Utlity</div>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>Map</div>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>More</div>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>Buy</div>
-                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase ml-2 cursor-pointer'>Connect</div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mr-2 cursor-pointer'
+                    onClick={() => toggleTab(2)}>
+                      Mint
+                    </div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'
+                    onClick={() => toggleTab(3)}>
+                      Utlity
+                    </div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'
+                    onClick={() => toggleTab(4)}>
+                      Map
+                    </div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'
+                    onClick={() => toggleTab(5)}>
+                      More
+                    </div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'
+                    onClick={() => toggleTab(6)}>
+                      Buy
+                    </div>
+                    <div className='px-3 py-1 text-[12px] bg-black hover:bg-green-500 text-white font-medium rounded-sm uppercase ml-2 cursor-pointer'>
+                      Connect
+                    </div>
                   </div>
                 </div>
               </div>
 
               {/* Contents */}
+
+              {toggleState === 1 ? (
+              <div className='absolute top[100px]'>
+                <h1 className='text-lg text-light'>Home page</h1>
+              </div>
+              ) :toggleState === 1 ? (
+                <div className='absolute top[100px]'>
+                <h1 className='text-lg text-light'>Mint page</h1>
+              </div>
+              ) : (
+               <div className='absolute top[100px]'>
+                <h1 className='text-lg text-light'>Other pages</h1>
+              </div> 
+              )
+              }
 
                
               
