7aa191e341943c7c0142df3cd5e84b40b3dfb211
Codehash001 <codehashblockchain@gmail.com>
2023-02-26 13:20:14 +0530

    update

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer(){
 		  <div className='flex items-center jutify-start w-full h-auto mt-6'>
 
 		    
-		    <h1 className='text-sm mx-4'>&#169;2023 RARE STUDIOS NEW YORK. All Rights Reserved.</h1>
+		    <h1 className='text-sm'>&#169;2023 RARE STUDIOS NEW YORK. All Rights Reserved.</h1>
 		  </div>
 		  
 		</div>
