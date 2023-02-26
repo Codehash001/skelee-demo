b0abbf1a9c33108ac60b1d951dd7fb61556cae17
Codehash001 <codehashblockchain@gmail.com>
2023-02-26 22:20:09 +0530

    css fix

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,14 @@
 export default function Footer(){
     return(
-        <div className=" bg-transparent w-full h-auto md:px-16 px-4">
+        <div className=" bg-transparent w-full h-auto md:px-16 px-2">
 
-<footer class="p-4 sm:p-6 text-white font-Kanit">
+<footer class="p-1 md:p-6 text-white font-Kanit">
 	<div className='flex items-center jutify-between w-full h-auto'>
 		<div className='flex flex-col items-start jutify-between w-full h-auto'>
 		
 		  <img src='/Skelee-logo.png' className='md:h-[40px] h-[25px] w-auto'/>
 		  
-		  <div className='flex items-center jutify-start w-full h-auto mt-6'>
+		  <div className='flex items-center jutify-start w-full h-auto md:mt-6 mt-3'>
 
 		    
 		    <h1 className='md:text-sm text-xs'>&#169;2023 RARE STUDIOS NEW YORK. All Rights Reserved.</h1>
