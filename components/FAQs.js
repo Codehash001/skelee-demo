93b971248e868cfaa5582da5fb256adfacd29832
Codehash001 <codehashblockchain@gmail.com>
2023-02-26 22:15:06 +0530

    css fix

--- components/FAQs.js
+++ components/FAQs.js
@@ -21,14 +21,14 @@ export default function FAQs() {
     <div id='faqs'className='md:mx-16 mx-2 p-6 rounded-lg text-white'>
       <h1 className='md:text-[40px] text-[30px] font-Kanit text-white text-center uppercase font-semibold tracking-widest mb-8'> FAQ</h1>
       <Accordion open={open === 1} animate={customAnimation}>
-        <AccordionHeader onClick={() => handleOpen(1)} className=' dark:text-white  text-[21px]'>
+        <AccordionHeader onClick={() => handleOpen(1)} className='text-white  text-[21px]'>
           What is Skelee nft?
         </AccordionHeader>
         <AccordionBody className='text-justify text-gray-300 text-[20px]'>
          description
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 2} animate={customAnimation} className=' dark:text-white  text-[22px]'>
+      <Accordion open={open === 2} animate={customAnimation} className='text-white  text-[22px]'>
         <AccordionHeader onClick={() => handleOpen(2)}>
           What are the benifits of holding a Skelee?
         </AccordionHeader>
@@ -36,7 +36,7 @@ export default function FAQs() {
         description
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 3} animate={customAnimation} className=' dark:text-white  text-[22px]'>
+      <Accordion open={open === 3} animate={customAnimation} className='text-white  text-[22px]'>
         <AccordionHeader onClick={() => handleOpen(3)}>
           Who can mint Skelee?
         </AccordionHeader>
@@ -44,7 +44,7 @@ export default function FAQs() {
           description
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 4} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 4} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(4)}>
           How do I claim my Skelee Boots?
         </AccordionHeader>
@@ -52,7 +52,7 @@ export default function FAQs() {
         description
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 5} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 5} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(5)}>
           How will minting work?
         </AccordionHeader>
@@ -60,7 +60,7 @@ export default function FAQs() {
         description
         </AccordionBody>
       </Accordion>
-      <Accordion open={open === 6} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 6} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(6)}>
           Are Skelee Nfts on ERC-721 or ERC-1155?
         </AccordionHeader>
@@ -69,7 +69,7 @@ export default function FAQs() {
         </AccordionBody>
       </Accordion>
 
-      <Accordion open={open === 7} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 7} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(7)}>
           Where can I view my SKELEE after purchasinh it?
         </AccordionHeader>
@@ -78,7 +78,7 @@ export default function FAQs() {
         </AccordionBody>
       </Accordion>
 
-      <Accordion open={open === 8} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 8} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(8)}>
           Will there be another Skelee's in the future?
         </AccordionHeader>
@@ -87,7 +87,7 @@ export default function FAQs() {
         </AccordionBody>
       </Accordion>
 
-      <Accordion open={open === 9} animate={customAnimation} className=' dark:text-white text-[21px]'>
+      <Accordion open={open === 9} animate={customAnimation} className='text-white text-[21px]'>
         <AccordionHeader onClick={() => handleOpen(9)}>
           What is the royalty % ?
         </AccordionHeader>
