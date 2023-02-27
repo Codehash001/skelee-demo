ddf518dff8af43096bb9b437ebd60add489a27db
Codehash001 <codehashblockchain@gmail.com>
2023-02-28 02:11:39 +0530

    update

--- components/Utlity.js
+++ components/Utlity.js
@@ -14,7 +14,7 @@ export default function Utlity() {
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/Skelee-arrival.gif" className="w-[250px] h-[250px] rounded-lg"/>
           <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>1 0F 1 ART PIECE</h1>
-          <p className='text-sm p-1 max-w-screen-sm'>Each NFT has unique value and desirability.
+          <p className='text-sm p-1 md:w-auto w-srceen'>Each NFT has unique value and desirability.
             <br/> As it not only represents ownership of a unique digital
             <br/> assest but also reflects the artist who created it.
           </p>
@@ -22,7 +22,7 @@ export default function Utlity() {
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/immortal-championsleague.png" className="w-[250px] h-[250px] rounded-lg"/>
           <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Gaming champions league</h1>
-          <p className='text-sm p-1 max-w-screen-sm'>Skelee's gaming champion league for
+          <p className='text-sm p-1 md:w-auto w-srceen'>Skelee's gaming champion league for
             <br/>NFT holders is the ultimate battleground
             <br/> where skilled players complete for glory and
             <br/> valuble digital assets.
@@ -36,7 +36,7 @@ export default function Utlity() {
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/skelee-immortal-community.png" className="w-[250px] h-[250px] rounded-lg"/>
           <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Immortal Community</h1>
-          <p className='text-sm p-1 max-w-screen-sm'>Our immortal community is a lively
+          <p className='text-sm p-1 md:w-auto w-srceen'>Our immortal community is a lively
             <br/>and engaging space where late night gamers
             <br/>from around the world can connect, chat and share
             <br/>their passion for gaming
@@ -45,7 +45,7 @@ export default function Utlity() {
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/skelee-boot-utility-.png" className="w-[250px] h-[250px] rounded-lg"/>
           <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Argumented Reality</h1>
-          <p className='text-sm p-1 max-w-screen-sm'>Skelees argumented reality 'experience' will
+          <p className='text-sm p-1 md:w-auto w-srceen'>Skelees argumented reality 'experience' will
             <br/>provide a fully impressive
             <br/>adventure that seamlessly blends
             <br/>the digital and physical world.
