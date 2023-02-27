3031af1b782e3549ec2ee1e86017b391e98c871b
Codehash001 <codehashblockchain@gmail.com>
2023-02-27 17:37:12 +0530

    update

--- components/Utlity.js
+++ components/Utlity.js
@@ -13,7 +13,7 @@ export default function Utlity() {
         
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/Skelee-arrival.gif" className="w-[250px] h-[250px] rounded-lg"/>
-          <h1 className='my-1 uppercase'>1 0F 1 ART PIECE</h1>
+          <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>1 0F 1 ART PIECE</h1>
           <p className='text-sm'>Skelee was created by a world class artist.
             <br/>Each NFT has uniqu value and desirability.
             <br/> as it not only represents ownership of a unique digital
@@ -22,7 +22,7 @@ export default function Utlity() {
         </div>
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/skelee-gaming-champions-league.png" className="w-[250px] h-[250px] rounded-lg"/>
-          <h1 className='my-1 uppercase'>Gaming champions league</h1>
+          <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Gaming champions league</h1>
           <p className='text-sm'>Skelee's gaming champion league for
             <br/>NFT holders is the ultimate battleground
             <br/> where skilled players complete for glory and
@@ -36,7 +36,7 @@ export default function Utlity() {
         
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/skelee-immortal-community.png" className="w-[250px] h-[250px] rounded-lg"/>
-          <h1 className='my-1 uppercase'>Immortal Community</h1>
+          <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Immortal Community</h1>
           <p className='text-sm'>Our immortal community is a lively
             <br/>and engaging space where late night gamers
             <br/>from around the world can connect, chat and share
@@ -45,7 +45,7 @@ export default function Utlity() {
         </div>
         <div className="w-[500px] flex flex-col items-center justify-center p-6">
           <img src="/skelee-skelee-boot.png" className="w-[250px] h-[250px] rounded-lg"/>
-          <h1 className='my-1 uppercase'>Argumented Reality</h1>
+          <h1 className='my-1 uppercase text-[#3ae516] text-[25px]'>Argumented Reality</h1>
           <p className='text-sm'>Skelees argumented reality 'experience' will
             <br/>provide a fully impressive and interactive gameplay
             <br/>adventure thatseamlessly blends
