c74ad27c9d72fd0f6f6c1ce9ba18b54fbf6600e6
Codehash001 <codehashblockchain@gmail.com>
2023-02-28 18:04:42 +0530

    update

--- components/countdown.js
+++ components/countdown.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export default function Countdown(){
 
 const calculateTimeLeft = () => {
-    const difference = +new Date("2022-02-28T18:20:00+05:30") - +new Date();
+    const difference = +new Date("2023-03-02T12:09:05+00:00") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -27,7 +27,7 @@ const calculateTimeLeft = () => {
 	
         return(
      <>
-     <div>
+     <div className="font-LuckiestGuy text-white w-ful h-full text-xl text-center">
       <p>
         <span>{timeLeft.hours}</span>
         <span>:</span>
