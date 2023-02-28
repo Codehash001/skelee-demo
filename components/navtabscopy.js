f7bb92d3431ad60d3bb9dd7145faf60647fed150
Codehash001 <codehashblockchain@gmail.com>
2023-02-28 10:06:24 +0530

    update

--- components/navtabscopy.js
+++ components/navtabscopy.js
@@ -130,11 +130,13 @@ export default function Navtabs (){
      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#FFFFFF"><path d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"></path></svg>
      </div>
      </a>
+     {/*
      <a href='/'>
      <div className='px-5 py-1 text-[13px] bg-black hover:bg-gray-700 text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'>
      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#FFFFFF"><circle cx="9.67" cy="13" r="1.001"></circle><path d="M14.09 15.391A3.28 3.28 0 0 1 12 16a3.271 3.271 0 0 1-2.081-.63.27.27 0 0 0-.379.38c.71.535 1.582.809 2.471.77a3.811 3.811 0 0 0 2.469-.77v.04a.284.284 0 0 0 .006-.396.28.28 0 0 0-.396-.003zm.209-3.351a1 1 0 0 0 0 2l-.008.039c.016.002.033 0 .051 0a1 1 0 0 0 .958-1.038 1 1 0 0 0-1.001-1.001z"></path><path d="M12 2C6.479 2 2 6.477 2 12c0 5.521 4.479 10 10 10s10-4.479 10-10c0-5.523-4.479-10-10-10zm5.859 11.33c.012.146.012.293 0 .439 0 2.24-2.609 4.062-5.83 4.062s-5.83-1.82-5.83-4.062a2.681 2.681 0 0 1 0-.439 1.46 1.46 0 0 1-.455-2.327 1.458 1.458 0 0 1 2.063-.063 7.145 7.145 0 0 1 3.899-1.23l.743-3.47v-.004A.313.313 0 0 1 12.82 6l2.449.49a1.001 1.001 0 1 1-.131.61L13 6.65l-.649 3.12a7.123 7.123 0 0 1 3.85 1.23 1.46 1.46 0 0 1 2.469 1c.01.563-.307 1.08-.811 1.33z"></path></svg>
      </div>
      </a>
+     */}
      <div className={toggleState === 2 ? 'px-3 py-1 text-[13px] bg-black text-white font-medium rounded-sm uppercase mx-2 cursor-pointer' : 'px-3 py-1 text-[13px]  bg-black text-white font-medium rounded-sm uppercase mx-2 cursor-pointer'}
      onClick={() => toggleTab(2)}>
        Connect
@@ -178,8 +180,8 @@ className='w-auto h-[30px] cursor-pointer'
         />
       </a>
         <div onClick={handleNav}
-        className='rounded-full hover:shadow-lg hover:shadow-gray-600 p-3 cursor-pointer bg-white '>
-          <AiOutlineClose size={18} />
+        className='rounded-full hover:shadow-lg hover:shadow-gray-600 p-3 cursor-pointer bg-white border border-white '>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill='#000000'><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
         </div>
     </div>
     <div className='flex flex-col items-start text-start mt-4 text-white'>
@@ -205,11 +207,12 @@ className='w-auto h-[30px] cursor-pointer'
       <div className='w-full uppercase py-2 border-b-2 border-white tracking-wider font-semibold text-[18px]'>
       Instagram</div>
       </a>
+      {/*
       <a target='_blank' rel="noreferrer" href='/' className='w-full'>
       <div className='w-full uppercase py-2 border-b-2 border-white tracking-wider font-semibold text-[18px]'>
       Reddit</div>
       </a>
-      {/*
+      
       <div className='w-full uppercase py-2 border-b-2 border-white tracking-wider font-semibold text-[18px]'>
       Rare studios new york</div> 
       */}
